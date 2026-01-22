382e753cdd80dc603e7a9ca8a3ab5192371c54e9
Jacob Tennenbaum <Jacobdten@gmail.com>
2026-01-21 21:50:17 -0500

    forgot to save something lol

--- bclogit/R/zzz.R
+++ bclogit/R/zzz.R
@@ -1,7 +1,3 @@
-if (getRversion() >= "2.15.1") {
-    utils::globalVariables(c("_bclogit_process_matched_pairs_cpp"))
-}
-
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
         paste("Welcome to bclogit v",
